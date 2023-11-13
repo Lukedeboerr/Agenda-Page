5d9261d69f13d72d6f2bf302c82f7e7bedb602c8
Luke <165249@student.horizoncollege.nl>
2023-11-13 11:22:02 +0100

    basics homepage

---
+++ Homepage/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Home - Huisplanner</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">
+            <img src="path/to/your/logo.png" width="30" height="30" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item px-5">
+                    <a class="nav-link" href="booking.html">Booking</a>
+                </li>
+                <li class="nav-item px-5">
+                    <a class="nav-link" href="#">Link 1</a>
+                </li>
+                <li class="nav-item px-5">
+                    <a class="nav-link" href="#">Link 2</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item px-5">
+                    <a class="nav-link btn btn-secondary" href="sign-in.php">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="pt-5">Welcome to Huisplanner</h1>
+                <p class="lead">Your go-to platform for managing your house plans and schedules.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Add your content and additional sections here -->
+
+    <footer class="fixed-bottom bg-dark text-white text-center py-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>E-mail: info@huisplanner.com</p>
+                    <p>Phone number: 06-123456789</p>
+                </div>
+                <div class="col-md-6">
+                    <form>
+                        <div class="form-row align-items-center">
+                            <div class="col-auto">
+                                <label class="sr-only" for="questionInput">Ask a question</label>
+                                <input type="text" class="form-control mb-2" id="questionInput" placeholder="Ask a question">
+                            </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-secondary mb-2">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        &copy; 2023 Huisplanner. All rights reserved.
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
